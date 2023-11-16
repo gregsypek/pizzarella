cafddd1852b05e09ce026936f7ff41e053c82779
gregsypek <47268138+gregsypek@users.noreply.github.com>
2023-11-16 20:52:26 +0100

    add cartReducer to store

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  cart:[
+    {
+      pizzaId:12,
+      name:"Margarita",
+      quantity:2,
+      unitPrice:15,
+      totalPrice:23,
+    }
+  ],
+}
+
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers:{
+    addItem(state, action) {
+      //payload = newItem
+      state.cart.push(action.payload)
+    },
+    deleteItem(state, action) {
+      //payload = pizzaId
+      state.cart = state.cart.filter(item=>item.pizzaId !== action.payload)
+    },
+    increaseItemQuantity(state, action) {
+      //payload = pizzaId
+      const item = state.cart.find(item=>item.pizzaId === action.payload)
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQuantity(state, action) {
+        //payload = pizzaId
+        const item = state.cart.find(item=>item.pizzaId === action.payload)
+
+        item.quantity--;
+        item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state) {
+      state.cart = []
+    },
+  }
+})
+
+export const {addItem,deleteItem,increaseItemQuantity, decreaseItemQuantity, clearCart}= cartSlice.actions;
+
+
+export default  cartSlice.reducer 
